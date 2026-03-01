085f378e4a07a373a96a2529c70583faf3f8b153
stevenzsky-maker <steven.z.sky@gmail.com>
2026-03-01 22:39:41 +0800

    增加配置

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js - 网站配置文件
+const APP_CONFIG = {
+    // 高德地图API Key（替换成你自己的）
+    AMAP_KEY: "e3418a2006816f412deec04d3e4d3d50",
+    // 可扩展其他配置
+    SITE_NAME: "CityFeet",
+    VERSION: "1.0.0"
+};
